09bff8050eb5cd2fdb4aae3223cb5ec24a4fbad0
JVP15 <jordanvonpordan@gmail.com>
2020-04-22 20:05:04 -0700

    Added training stuff to main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include "HiddenMarkovModel.h"
+
+Observation returnObservation(char x)
+{
+    return (int)x - 87;
+}
 
 int main()
 {
@@ -20,11 +26,60 @@ int main()
     ObservationSequence O = {0, 1, 0, 2};
     double score = hmm.scoreStateSequence(O);
     std::cout << score << std::endl;
-    StateSequence optimal = hmm.optimalStateSequence(O);
+    /*StateSequence optimal = hmm.optimalStateSequence(O);
 
     for(auto i: optimal)
-        std::cout << i << " ";
+        std::cout << i << " ";*/
 
-    hmm = *new HiddenMarkovModel(O, 2, 3);
-    hmm.train(O, 3);
+
+
+
+    int HEADER_SIZE = 15;
+    std::string line;
+    ObservationSequence O2;
+    //this should be an absolute path to corpus.dos, and A is the only starting letter of files that I am iterating through.
+    std::string filebase = "C:\\Users\\jorda\\Documents\\C++\\HMM\\Brown\\corpus.dos\\A";
+    std::string filenames[40];
+
+    for(int i = 1; i < 40; i++)
+    {
+        filenames[i] = filebase;
+        if(i < 10)
+            filenames[i] += "0";
+        filenames[i] += std::to_string(i);
+        //::cout << filenames[i] << std::endl;
+    }
+
+    for(int i = 1; i < 40; i++)
+    {
+        std::ifstream file;
+        file.open(filenames[i]);
+        if(!file.is_open())
+            std::cout << "oops\n";
+
+        while(getline(file, line))
+        {
+            for(int i = HEADER_SIZE; i < line.length(); i++)
+            {
+                char c = line[i];
+
+                if(c == ' ')
+                    O2.push_back(27);
+                else
+                {
+                    c = tolower(c);
+                    Observation o = returnObservation(c);
+                    if (o < 27 && o >= 0)
+                        O2.push_back(o);
+                }
+            }
+        }
+        file.close();
+    }
+
+
+    HiddenMarkovModel hmm2(O2, 2, 27);
+    hmm2.train(O2, 100);
+
+    std::cout << "finished trainign HMM";
 }
