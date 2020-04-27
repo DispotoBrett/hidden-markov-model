62429b6d0570f60ac5962ff15852bb5f9a0624b6
JVP15 <jordanvonpordan@gmail.com>
2020-04-26 20:57:16 -0700

    Added training, validation, and testing preprocessing steps

--- Python/Preprocesser.py
+++ Python/Preprocesser.py
@@ -4,7 +4,22 @@ import numpy as np
 
 parent_dir = os.path.dirname(os.getcwd())
 opcode_dir = parent_dir + '/Opcodes'
-NUM_UNIQUE_OPCODES = 32
+MAX_UNIQUE_OPCODES = 32
+MAX_FAMILIES = 5
+TRAIN_SIZE = 1000 #temporary value for quick testing
+VALIDATION_SIZE = int(TRAIN_SIZE * .2)
+TEST_SIZE = int(TRAIN_SIZE * .1)
+
+def convert_file_to_symbol_arr(file_path, symbol_dict):
+    symbols = []
+    if os.path.exists(file_path) and file_path.endswith('.txt'):
+        with open(file_path, 'r') as file:
+            opcode_reader = csv.reader(file)
+            for opcode in opcode_reader:
+                opcode = opcode[0]
+                symbols.append(symbol_dict.get(opcode, MAX_UNIQUE_OPCODES))
+
+    return symbols
 
 def count_opcodes():
     for family in os.scandir(opcode_dir):
@@ -42,16 +57,55 @@ def largest_families():
         if family.is_dir():
             print('Working on family ' + family.name)
 
-            family_iterable = os.scandir()
             num_files[family.name] = len([file for file in os.scandir(family)])
 
     families = list(num_files.keys())
     families.sort(reverse=True, key=lambda f: num_files[f])
     np.save(opcode_dir + '/' + 'sorted_families.npy', np.asarray(families))
 
-#count_opcodes()
-#popular_opcodes(NUM_UNIQUE_OPCODES)
-#largest_families()
+def setup_processed_dataset():
+    with open(opcode_dir + '/' + 'preprocessed_families.txt', 'w') as preprocessed_families:
+        sorted_families = np.load(opcode_dir + '/' + 'sorted_families.npy')
+        for i in range(MAX_FAMILIES):
 
-x = np.load(opcode_dir + '/' + 'sorted_families.npy')
-print(x)
+            family_name = sorted_families[i]
+            family_dir = opcode_dir + '/' + family_name
+            symbol_dict = np.load(opcode_dir + '/' + family_name + '/' + 'opcode_symbol.npy', allow_pickle=True).item()
+
+            preprocessed_families.write(family_name + '\n')
+
+            train_arr = np.empty(TRAIN_SIZE)
+            train_elements = 0
+            val_arr = np.empty(VALIDATION_SIZE)
+            val_elements = 0
+            test_arr = np.empty(TEST_SIZE)
+            test_elements = 0
+
+            for virus in os.scandir(family_dir):
+                symbols = convert_file_to_symbol_arr(virus.path, symbol_dict)
+                for symbol in symbols:
+                    if train_elements < TRAIN_SIZE:
+                        train_arr[train_elements] = symbol
+                        train_elements += 1
+                    elif val_elements < VALIDATION_SIZE:
+                        val_arr[val_elements] = symbol
+                        val_elements += 1
+                    elif test_elements < TEST_SIZE:
+                        test_arr[test_elements] = symbol
+                        test_elements += 1
+                    else:
+                        break
+
+            format = '%d'
+            np.savetxt(fname=opcode_dir + '/' + family_name + '/' + 'train.txt', X=train_arr, fmt=format)
+            np.savetxt(fname=opcode_dir + '/' + family_name + '/' + 'val.txt', X=val_arr, fmt=format)
+            np.savetxt(fname=opcode_dir + '/' + family_name + '/' + 'test.txt', X=test_arr, fmt=format)
+
+#count_opcodes()
+#popular_opcodes(MAX_UNIQUE_OPCODES)
+#largest_families()
+setup_processed_dataset()
+
+
+
+
