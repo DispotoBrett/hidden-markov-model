d5e8a6d104ad13b9ef82944b48689ea1d6f7cb01
JVP15 <jordanvonpordan@gmail.com>
2020-04-28 20:08:18 -0700

    I think I'm getting results now

--- java/Boosting.java
+++ java/Boosting.java
@@ -37,14 +37,25 @@ public class Boosting
 		Files.lines(processedFamiliesFile).forEach(s -> processedFamilies.add(s));
 		
 		
-		for(String family: processedFamilies)
+		//testManyFamilies(processedFamilies, opcodeDir);
+		testOneFamily(processedFamilies.get(2), opcodeDir, 5);
+		
+		System.out.print("Testing done");
+	}
+	
+	public static void testManyFamilies(ArrayList<String> families, String opcodeDir)
+	{
+		
+		for(String family: families)
 		{
 			String datasetFiles = opcodeDir + "/" + family + "/%s.txt";
 			ArrayList<Integer> train = getObservationSequenceFromFile(String.format(datasetFiles, "train"));
 			ArrayList<Integer> validate = getObservationSequenceFromFile(String.format(datasetFiles, "val"));
 			ArrayList<Integer> test = getObservationSequenceFromFile(String.format(datasetFiles, "test"));
 			ArrayList<Integer> test2 = getObservationSequenceFromFile(String.format(datasetFiles, "test2"));
-			HiddenMarkovModel hmm = new HiddenMarkovModel(train, 2, MAX_UNIQUE_OPCODES + 1, 0);
+			int seed =(int) System.nanoTime();
+			
+			HiddenMarkovModel hmm = new HiddenMarkovModel(train, 2, MAX_UNIQUE_OPCODES + 1, seed);
 			System.out.println("HMM for " + family);
 			hmm.train(train, validate, 100);
 			hmm.prettyPrint();
@@ -53,8 +64,29 @@ public class Boosting
 			System.out.println("Testing Score = " + hmm.scoreStateSequence(test));
 			System.out.println("Testing Score 2 = " + hmm.scoreStateSequence(test2) + "\n");
 		}
-		
-		System.out.print("Loading done");
+	}
+	
+	public static void testOneFamily(String family, String opcodeDir, int maxIterations)
+	{
+		for(int i = 0; i < maxIterations; i++)
+		{
+			String datasetFiles = opcodeDir + "/" + family + "/%s.txt";
+			ArrayList<Integer> train = getObservationSequenceFromFile(String.format(datasetFiles, "train"));
+			ArrayList<Integer> validate = getObservationSequenceFromFile(String.format(datasetFiles, "val"));
+			ArrayList<Integer> test = getObservationSequenceFromFile(String.format(datasetFiles, "test"));
+			ArrayList<Integer> test2 = getObservationSequenceFromFile(String.format(datasetFiles, "test2"));
+			int seed =(int) System.nanoTime();
+			System.out.println(seed);
+			HiddenMarkovModel hmm = new HiddenMarkovModel(train, 2, MAX_UNIQUE_OPCODES + 1, seed);
+			System.out.println("HMM for " + family);
+			hmm.train(train, train, 100);
+			hmm.prettyPrint();
+			System.out.println("Training Score = " + hmm.scoreStateSequence(train));
+			System.out.println("Validate Score = " + hmm.scoreStateSequence(validate));
+			System.out.println("Testing Score = " + hmm.scoreStateSequence(test));
+			System.out.println("Testing Score 2 = " + hmm.scoreStateSequence(test2) + "\n");
+			hmm.saveToFile(String.format(datasetFiles, "hmm" + i + ".txt"));
+		}
 	}
 	
 }
